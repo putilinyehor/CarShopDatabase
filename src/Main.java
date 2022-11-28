c5ebe2468bba41e612677e6b61d7dedf40b86ca2
putilinyehor <egputilin@gmail.com>
2022-11-29 00:51:50 +0100

    New code added

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,18 @@ import cardata.DrivetrainType;
 import persondata.Address;
 import persondata.Customer;
 import persondata.Manager;
+import systemdata.InputOutput;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    // Lists initialisation
+    List<Manager> managersList = new ArrayList<Manager>();
+    List<Customer> customersList = new ArrayList<Customer>();
+    List<Car> carsList = new ArrayList<Car>();
+
     private static Scanner scanner;
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
@@ -16,8 +24,12 @@ public class Main {
         Manager managerTemplate = new Manager("Joseph", "Seed", "Stark", dateOfBirth, "1238712731784", addressTemplate1, 2.2F);
         Customer customerTemplate = new Customer("John", "Ares", "The Mighty", dateOfBirth, "223588564856", addressTemplate2);
         Car carTemplate = new Car("Mercedes-Benz GLC 250", "Mercedes-Benz", 2016, DrivetrainType.AWD, 35000, managerTemplate);
-        System.out.println(managerTemplate.stringManagerInfo());
-        System.out.println(carTemplate.stringCarInfo());
-        System.out.println(customerTemplate.stringCustomerInfo());
+        //System.out.println(managerTemplate.stringManagerInfo());
+        //System.out.println(carTemplate.stringCarInfo());
+        //System.out.println(customerTemplate.stringCustomerInfo());
+
+        InputOutput.writeManagersFile();
+        InputOutput.readManagersFile();
+
     }
 }
---
+++ src/systemdata/InputOutput.java
@@ -0,0 +1,63 @@
+package systemdata;
+
+import java.io.*;
+
+public class InputOutput {
+    private static String directory = System.getProperty("user.dir");
+    private static String managersFileName = "managers.txt";
+    private static String customersFileName = "customers.txt";
+    private static String carsFileName = "managers.txt";
+    private static String absolutePath = directory + File.separator; // + fileName !
+
+    /**
+     *
+     * @return
+     */
+    public static boolean writeManagersFile() {
+        try (FileWriter fileWriter = new FileWriter(absolutePath + managersFileName)) {
+            String fileContent = "Simple text";
+            fileWriter.write(fileContent);
+            fileWriter.close();
+        } catch (IOException e) {
+            // Handle exception
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean writeCustomersFile() {
+        return false;
+    }
+
+    public static boolean writeCarsFile() {
+        return false;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static boolean readManagersFile() {
+        try {
+            FileWriter fileWriter = new FileWriter("MyFile.txt", true);
+            fileWriter.write("Hello World");
+            fileWriter.write("\r\n");   // write new line
+            fileWriter.write("Good Bye!");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean readCustomersFile() {
+        return false;
+    }
+
+    public static boolean readCarsFile() {
+
+        return false;
+    }
+}
